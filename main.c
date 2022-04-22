bc990d8345a16d018b699ed5c64a61401afbea00
jariisohanni <jari.isohanni@gmail.com>
2022-04-22 08:48:24 +0300

    Some comments added

--- main.c
+++ main.c
@@ -14,15 +14,18 @@ int main(int argc, char *argv[])
 {
     printf("Our first appliction!\n"); //Prints text inside quotes
 
+    //This how we initialise variables
     int x1 = 0;
     int x2 = 3;
 
     printf("x1 %d \n", x1);
     printf("x2 %d \n", x2);
 
+    //Printing of variables
     short a = 2, b = 4, c = 8;
     printf("a %d, b %d, c %d,   \n", a,b,c);
 
+    //Some arithmetics
     int x = 3;
     int y = 5;
     y = (x+3) * ( x/(y-4) ) ;
@@ -34,9 +37,11 @@ int main(int argc, char *argv[])
     x++; //Increases x by 1, is same as x = x + 1
     x--; //Increases x by 1, is same as x = x - 1
 
+    //Finally functions
     int v = multiply(2,3);
     printf("v is: %d \n", v);
 
+    //For loop + command line arguments
     for (int i = 0; i < argc; i++)
     {
         printf("argument %d is %s\n", i, argv[i]);
